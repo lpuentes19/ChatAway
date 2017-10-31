24bbb8716d6a8a69b2b45a45c4bf171369f05ef4
lpuentes19 <luey.puentes@gmail.com>
2017-10-30 18:16:38 -0600

    Initial Commit

---
+++ ChatAway/Extensions/UIColor+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Extension.swift
+//  ChatAway
+//
+//  Created by Luis Puentes on 10/30/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import Foundation
---
+++ ChatAway/ViewControllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  ChatAway
+//
+//  Created by Luis Puentes on 10/30/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor
+    }
+}
